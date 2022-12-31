3589846fdf9b33690bb8d263ba8974e926ac673b
tunahanbozkurt <t.bozkurt5775@gmail.com>
2023-01-01 00:49:07 +0300

    base classes

---
+++ app/src/main/java/com/example/rickandmorty_xml/presentation/base/BaseFragment.kt
@@ -0,0 +1,47 @@
+package com.example.rickandmorty_xml.presentation.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB: ViewBinding, VM: BaseViewModel>(
+    private val bindingInflater: (inflater: LayoutInflater) -> VB,
+    private val viewModelClass: Class<VM>
+
+): Fragment() {
+
+    private var _binding: VB? = null
+    private var _viewModel: VM? = null
+
+    val binding: VB
+        get() = _binding as VB
+
+    val viewModel: VM
+        get() = _viewModel as VM
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _viewModel = ViewModelProvider(this)[viewModelClass]
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = bindingInflater.invoke(inflater)
+        if (_binding == null) {
+            throw IllegalArgumentException("Binding cannot be null")
+        }
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/rickandmorty_xml/presentation/base/BaseViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.rickandmorty_xml.presentation.base
+
+import androidx.lifecycle.ViewModel
+
+abstract class BaseViewModel: ViewModel() {
+}
